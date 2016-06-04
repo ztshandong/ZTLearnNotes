5ba7af13424eb7720150821e68cf29c78b3c2b57
张涛 <ztshandong@gmail.com>
2016-06-04 20:08:45 +0800

    2016.6.4

---
+++ ZTLearnNotes/ZTERP.CommonClass/Common/Loginer.cs
@@ -0,0 +1,141 @@
+﻿
+using System;
+
+namespace ZTERP.CommonClass
+{   
+    [Serializable]
+    public class Loginer
+    {
+        public Loginer()
+        { }
+        public Loginer(Loginer user)
+        {
+            this.Account = user.Account;
+            this.AccountName = user.AccountName;
+            this.DataSetID = user.DataSetID;
+            this.DataSetName = user.DataSetName;
+            this.DBName = user.DBName;
+            this.DBServer = user.DBServer;
+            this.DBUserID = user.DBUserID;
+            this.DBUserPwd = user.DBUserPwd;
+            this.FlagAdmin = user.FlagAdmin;
+            this.IP = user.IP;
+            this.LoginTime = user.LoginTime;
+            this.MAC = user.MAC;
+            this.Password = user.Password;
+            this.DataSetIdAll = user.DataSetIdAll;
+        }
+        
+        private static Loginer _User = new Loginer();
+                
+        public static Loginer CurrentUser
+        {
+            get { return _User; }
+            set { _User = value; }
+        }
+        
+        private string _Account = "";
+        private string _Password = "";
+        private string _DataSetID = "";
+        private string _DataSetName = "";
+        private string _DBServer = "";
+        private string _DBName = "";
+        private string _DBUserID = "";
+        private string _DBUserPwd = "";
+        private string _IP = "";
+        private string _MAC = "";
+        private string _AccountName = "";
+        private DateTime _LoginTime;
+        private string _FlagAdmin = "N";
+        private string _LicenseID = "";
+        private string _DataSetIdAll = "";
+
+        /// <summary>
+        /// 我的所有帐套编号，,SH,XA,
+        /// </summary>
+        public string DataSetIdAll { get { return _DataSetIdAll; } set { _DataSetIdAll = value; } }
+
+        /// <summary>
+        /// 许可证编号，客户识别码
+        /// </summary>
+        public string LicenseID { get { return _LicenseID; } set { _LicenseID = value; } }
+
+        /// <summary>
+        /// 登录的数据库服务器
+        /// </summary>
+        public string DBServer { get { return _DBServer; } set { _DBServer = value; } }
+
+        /// <summary>
+        /// 登录的数据库
+        /// </summary>
+        public string DBName { get { return _DBName; } set { _DBName = value; } }
+
+        /// <summary>
+        /// 数据库用户
+        /// </summary>
+        public string DBUserID { get { return _DBUserID; } set { _DBUserID = value; } }
+
+        /// <summary>
+        /// 数据库密码
+        /// </summary>
+        public string DBUserPwd { get { return _DBUserPwd; } set { _DBUserPwd = value; } }
+
+        /// <summary>
+        /// 用户帐号，登录帐号
+        /// </summary>
+        public string Account { get { return _Account; } set { _Account = value; } }
+
+        /// <summary>
+        /// 登录密码
+        /// </summary>
+        public string Password { get { return _Password; } set { _Password = value; } }
+
+        /// <summary>
+        /// 当前登录的网点
+        /// </summary>
+        public string DataSetID { get { return _DataSetID; } set { _DataSetID = value; } }
+
+        /// <summary>
+        /// 当前登录的网点名称
+        /// </summary>
+        public string DataSetName { get { return _DataSetName; } set { _DataSetName = value; } }
+
+        /// <summary>
+        /// 当前用户的IP
+        /// </summary>
+        public string IP { get { return _IP; } set { _IP = value; } }
+
+        /// <summary>
+        /// 当前网卡信息
+        /// </summary>
+        public string MAC { get { return _MAC; } set { _MAC = value; } }
+
+        #region 属性定义
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string AccountName { get { return _AccountName; } set { _AccountName = value; } }
+
+        /// <summary>
+        /// 登录时间
+        /// </summary>
+        public DateTime LoginTime { get { return _LoginTime; } set { _LoginTime = value; } }
+
+        /// <summary>
+        /// ADMIN标记, Y/N
+        /// </summary>
+        public string FlagAdmin { get { return _FlagAdmin; } set { _FlagAdmin = value; } }
+
+        /// <summary>
+        /// 是否ADMIN
+        /// </summary>
+        /// <returns></returns>
+        public bool IsAdmin() { return _FlagAdmin == "Y"; }
+
+        #endregion
+
+    }
+
+}
+
---
+++ ZTLearnNotes/ZTERP.DataAccessLayer/GetDBCommond.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Data.OracleClient;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Security;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using ZTERP.CommonClass;
+
+namespace ZTERP.DataAccessLayer
+{
+
+    public sealed class GetDataBaseType
+    {
+        private static readonly GetDataBaseType instance = new GetDataBaseType();
+
+        private GetDataBaseType() { }
+
+        public static GetDataBaseType Instance
+        {
+            get { return instance; }
+        }
+
+        public DataBaseType DbType
+        {
+            get            {                return _dbType;            }
+
+            set            {                _dbType = value;            }
+        }
+
+        private DataBaseType _dbType;
+        public DataBaseType CreateDataBaseType(string ClassName, object[] args)
+        {
+            _dbType= this.createDataBaseType(ClassName, args);
+            return _dbType;
+        }
+        private DataBaseType createDataBaseType(string ClassName, object[] args)
+        {
+            DataBaseType con = null;
+            string SpaceName = typeof(DALDLL).Assembly.GetName().Name;
+            BindingFlags bf = BindingFlags.CreateInstance | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public;
+
+            #region 方法一
+            //var asm = Assembly.LoadFile(typeof(DALDLL).Assembly.Location);
+            //Type T1 = asm.GetType();
+            //Type tmp = null;
+            //foreach (Type t1 in asm.DefinedTypes)
+            //{
+            //    if (t1.Name == ClassName)
+            //    {
+            //        tmp = t1;
+            //        break;
+            //    }
+            //}            
+            //con = tmp.InvokeMember(null, bf, null, null, args);
+            #endregion
+
+            //方法二
+            try
+            {
+                con = (DataBaseType)typeof(DALDLL).Assembly.CreateInstance(SpaceName + "." + ClassName, true,
+                        bf, null, args, null, null);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            //if (type== "MSSQL") con = ConnectDataBaseMSSQL.Instance;
+            //else if (type == "Oracle") con = ConnectDataBaseOracle.Instance;
+            //else if (type == "MySQL") con = ConnectDataBaseMySQL.Instance;
+            //else if (type == "Accese") con = ConnectDataBaseAccese.Instance;
+            return con;
+        }
+
+    }
+
+
+
+
+    public abstract class DataBaseType : DbProviderFactory, IServiceProvider
+    {
+        public virtual object GetService(Type serviceType)
+        {
+            return null;
+        }
+    }
+
+    public class DataBaseMSSQL : DataBaseType
+    {
+        public DataBaseMSSQL()
+        { }
+        public override DbCommand CreateCommand()
+        {
+            return new SqlCommand();
+        }
+
+        public override DbCommandBuilder CreateCommandBuilder()
+        {
+            return new SqlCommandBuilder();
+        }
+
+        public override DbConnection CreateConnection()
+        {
+            string CONN_FORMAT_STRING = "Server={0};Database={1};User ID={2};Password={3};Connection TimeOut={4};Pooling=true;Max Pool Size = 512;";
+            string sConn = String.Format(CONN_FORMAT_STRING, Loginer.CurrentUser.DBServer, Loginer.CurrentUser.DBName, Loginer.CurrentUser.DBUserID, Loginer.CurrentUser.DBUserPwd, 20);
+
+            return new SqlConnection(sConn);
+        }
+        public DbConnection CreateConnection(bool IsSysDB)
+        {
+            if (!IsSysDB)
+                return this.CreateConnection();
+            else
+                return new SqlConnection("连接系统库");
+        }
+
+        public override DbConnectionStringBuilder CreateConnectionStringBuilder()
+        {
+            return new SqlConnectionStringBuilder();
+        }
+
+        public override DbDataAdapter CreateDataAdapter()
+        {
+            return new SqlDataAdapter();
+        }
+
+        public override DbDataSourceEnumerator CreateDataSourceEnumerator()
+        {
+            return SqlDataSourceEnumerator.Instance;
+        }
+
+        public override DbParameter CreateParameter()
+        {
+            return new SqlParameter();
+        }
+
+        public override CodeAccessPermission CreatePermission(PermissionState state)
+        {
+            return new SqlClientPermission(state);
+        }
+
+    }
+    public class ConnectDataBaseMSSQL : DataBaseType
+    {
+        public static readonly ConnectDataBaseMSSQL Instance = new ConnectDataBaseMSSQL();
+
+        private ConnectDataBaseMSSQL()
+        {
+        }
+
+        public override DbCommand CreateCommand()
+        {
+            return new SqlCommand();
+        }
+
+        public override DbCommandBuilder CreateCommandBuilder()
+        {
+            return new SqlCommandBuilder();
+        }
+
+        public override DbConnection CreateConnection()
+        {
+            return new SqlConnection();
+        }
+
+        public override DbConnectionStringBuilder CreateConnectionStringBuilder()
+        {
+            return new SqlConnectionStringBuilder();
+        }
+
+        public override DbDataAdapter CreateDataAdapter()
+        {
+            return new SqlDataAdapter();
+        }
+
+        public override DbDataSourceEnumerator CreateDataSourceEnumerator()
+        {
+            return SqlDataSourceEnumerator.Instance;
+        }
+
+        public override DbParameter CreateParameter()
+        {
+            return new SqlParameter();
+        }
+
+        public override CodeAccessPermission CreatePermission(PermissionState state)
+        {
+            return new SqlClientPermission(state);
+        }
+
+    }
+    public class DataBaseOracle : DataBaseType
+    {
+        public static readonly DataBaseOracle Instance = new DataBaseOracle();
+
+        // 构造函数
+        private DataBaseOracle()
+        {
+        }
+        public override DbCommand CreateCommand()
+        {  // 创建具体产品
+            return new OracleCommand();
+        }
+        public override DbConnection CreateConnection()
+        {
+            string CONN_FORMAT_STRING = "Server={0};Database={1};User ID={2};Password={3};Connection TimeOut={4};Pooling=true;Max Pool Size = 512;";
+            string sConn = String.Format(CONN_FORMAT_STRING, Loginer.CurrentUser.DBServer, Loginer.CurrentUser.DBName, Loginer.CurrentUser.DBUserID, Loginer.CurrentUser.DBUserPwd, 20);
+
+            return new OracleConnection(sConn);
+        }
+    }
+    public class DataBaseMySQL : DataBaseType
+    {
+        public static readonly DataBaseMySQL Instance = new DataBaseMySQL();
+
+        // 构造函数
+        private DataBaseMySQL()
+        {
+
+        }
+    }
+    public class DataBaseAccese : DataBaseType
+    {
+        public static readonly DataBaseAccese Instance = new DataBaseAccese();
+
+        // 构造函数
+        private DataBaseAccese()
+        {
+        }
+        public override DbCommand CreateCommand()
+        {  // 创建具体产品
+            return new OleDbCommand();
+        }
+
+    }
+
+
+
+}
